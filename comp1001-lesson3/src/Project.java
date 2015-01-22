78f334809176b9412e1d283772ff14e073bcb6a7
JcBenard <niteburn@hotmail.com>
2015-01-22 16:25:21 -0500

    Added vehicle class

--- comp1001-lesson3/src/Project.java
+++ comp1001-lesson3/src/Project.java
@@ -1,12 +1,18 @@
 /**
  * @author Jc
- * @version 1 - Created basic project
+ * @version 2 - Added vehicle class
  * Program Description: Demo files for lesson 3
  */
+
 public class Project {
 
+	/**
+	 * Main method for project
+	 * @param args
+	 */
+	
 	public static void main(String[] args) {
-
+		Vehicle myVehicle = new Vehicle();
+	
 	}
-
 }

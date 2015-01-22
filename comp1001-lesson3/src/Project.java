ace7c836c89633b73c5c2fd8fc245efa22c66ec0
JcBenard <niteburn@hotmail.com>
2015-01-22 15:33:50 -0500

    Created basic Project

---
+++ comp1001-lesson3/src/Project.java
@@ -0,0 +1,12 @@
+/**
+ * @author Jc
+ * @version 1 - Created basic project
+ * Program Description: Demo files for lesson 3
+ */
+public class Project {
+
+	public static void main(String[] args) {
+
+	}
+
+}
